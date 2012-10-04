4627deb95db19fe83797e970c3eedb2e1d9ff4c7
erosen <erosen@wikimedia.org>
2012-10-04 14:35:25 -0700

    created package and setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name='gcat',
+    version='0.1.0',
+    description='Command-line interface for Google Drive',
+    url='http://www.github.com/embr/gcat',
+    author='Evan Rosen',
+    author_email='erosen@wikimedia.org',
+    entry_points = {
+        'console_scripts': [
+            'gcat = gcat:main',
+            ]
+        },
+    install_requires=[
+       "oauth2client >= 1.0",
+       "google-api-python-client >= 1.0",
+       "httplib2 >= 0.7.6",
+       ],
+    include_package_data = True
+    )
+
