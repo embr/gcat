91eeb0f8f51e51f36b95f7a2479f22ec6a0dfea7
Evan Rosen <erosen@wikimedia.org>
2012-12-06 02:04:09 +0000

    switch to parsing the excel files with pandas

--- gcat/__init__.py
+++ gcat/__init__.py
@@ -26,6 +26,8 @@ LOGLEVELS = {'DEBUG': logging.DEBUG,
              'ERROR': logging.ERROR,
              'CRITICAL': logging.CRITICAL}  
 
+logger = logging.getLogger(__name__)
+
 def default_options():
     defaults = {}
     defaults['store'] = os.path.expanduser('~/.gcat/store')
@@ -42,7 +44,7 @@ def load_config(opts):
                 config = yaml.load(f)
                 return dict(config)
         except IOError:
-            logging.error('Could not find config file at %s', yaml_name)
+            logger.error('Could not find config file at %s', yaml_name)
             sys.exit()
     else:
         return {}
@@ -58,7 +60,7 @@ def get_file(title, fmt='dict', **kwargs):
       * `dict`     : list of dicts (Default).
       * `xlrd`     : xlrd.Book (only full workbook is exported)
       * `list`     : list of lists
-      * `pandas`   : Pandas.DataFrame
+      * `pandas_excel`   : Pandas.ExcelFile, usefule for custom parsing
     For all formats other than `xlrd`, if no sheet name is given, get_file returns
     a dict with sheet names as keys and accordingly formatted rows as values.
     """
@@ -66,112 +68,52 @@ def get_file(title, fmt='dict', **kwargs):
     opts['title'] = title
     opts.update((k,v) for k, v in load_config(opts).items() if v is not None)
     opts.update((k,v) for k, v in kwargs.items() if v is not None)
-    logging.info('opts:\n%s', pprint.pformat(opts))
+    logger.info('opts:\n%s', pprint.pformat(opts))
 
     content = get_content(opts)
-    wb = xlrd.open_workbook(file_contents=content)
+    wb = pd.ExcelFile(StringIO.StringIO(content))
 
-    if fmt == 'xlrd':
-        if 'sheet' not in opts and 'sheet_id' not in opts:
-            return wb
-        else:
-            logger.warning('fmt: xlrd and sheet selection not supported, returning whole workbook')
-            return wb
-    else: # parse and handle other formats
-        parsed_wb = parse(wb, opts)
-        if fmt == 'list':
-            fmt_wb = parsed_wb
-        elif fmt == 'dict':
-            fmt_wb = fmt_as_dict(parsed_wb)
-        elif fmt == 'pandas':
-            fmt_wb = fmt_as_pandas(parsed_wb)
-        else:
-            raise ValueError('unkown format: %s' % fmt)
-        if len(fmt_wb) == 1:
-            return fmt_wb.values()[0]
-        else:
-            return fmt_wb
-
-
-def fmt_as_dict(parsed_wb):
-    fmt_wb = {}
-    for sheet_name, ws in parsed_wb.items():
-        labels = ws[0]
-        fmt_sheet = []
-        for row in ws[1:]:
-            dict_row = dict(zip(labels, row))
-            fmt_sheet.append(dict(filter(lambda (k,v): len(unicode(v)) > 0, dict_row.items())))
-        fmt_wb[sheet_name] = fmt_sheet
-    return fmt_wb
-
-
-def fmt_as_pandas(parsed_wb):
-    return {sn : pd.DataFrame(sheet) for sn, sheet in fmt_as_dict(parsed_wb).items()}
-
-
-def parse(wb, opts):
-    parsers = {
-        0 : lambda c : None,
-        1 : lambda c : c,
-        2 : lambda c : c,
-        3 : lambda c : datetime.datetime(*xlrd.xldate_as_tuple(c, wb.datemode)),
-        4 : lambda c : bool,
-        5 : lambda c : None,
-        6 : lambda c : None,
-        }
-
-    parsed_wb = {}
-    for sn in get_sheet_names(wb, opts):
-        ws = wb.sheet_by_name(sn)
-        parsed_ws = []
-        for rowx in range(ws.nrows):
-            parsed_ws.append([None for i in range(ws.ncols)])
-            for colx in range(ws.ncols):
-                    raw_val = ws.cell_value(rowx, colx)
-                    parser = parsers[ws.cell_type(rowx, colx)]
-                    try:
-                        parsed_val = parser(raw_val)
-                    except:
-                        logging.exception('exception when trying to convert cell(rowx=%d, colx=%d, colname=%d)', rowx, colx, ws.cell_value(0,colx))
-                        logging.exception('type(raw_val): %s, raw_val: %s,  parser: %s', 
-                                          type(raw_val),
-                                          raw_val.encode('utf-8') if isinstance(raw_val, unicode) else str(raw_val),
-                                          parser)
-                        parsed_val = None
-                    parsed_ws[rowx][colx] = parsed_val
-        parsed_wb[sn] = parsed_ws
-    return parsed_wb
-
-
-def get_sheet_names(wb, opts):
-    if 'sheet' in opts:
-        sns = [opts['sheet']]
-    elif 'sheet_index' in opts:
-        sns = [wb.sheet_names()[opts['sheet_index']]]
-    else:
-        sns = wb.sheet_names()
-    return sns
+    if fmt == 'pandas_excel':
+        return wb
     
+    try:
+        parsed_wb = {sheet_name : wb.parse(sheet_name) for sheet_name in wb.sheet_names}
+    except:
+        logger.exception('error parsing worksheet using pandas.ExcelFile.parse(sheet_name). '
+                         'Consider using pandas_excel and parsing the file yourself to have more control')
+    parsed_wb = parse(wb, opts)
+    if fmt == 'list':
+        fmt_wb = {name : list(df.itertuples()) for name, df in parsed_wb.items()}
+    elif fmt == 'dict':
+        fmt_wb = {name : [r[1].to_dict() for r in df.iterrows()] for name, df in parsed_wb.items()}
+    elif fmt == 'pandas':
+        fmt_wb = parsed_wb
+    else:
+        raise ValueError('unkown format: %s' % fmt)
+    if len(fmt_wb) == 1:
+        return fmt_wb.values()[0]
+    else:
+        return fmt_wb
 
 def get_content(opts):
     cache = shelve.open(opts['cache'])
     if opts['usecache'] and opts['title'] in cache:
         content = cache[opts['title']]
     else:
-        logging.debug('computing from scratch')
+        logger.debug('computing from scratch')
         service = get_service(opts)
         files = service.files()
         try:
             res = files.list().execute()
         except errors.HttpError, error:
-            logging.error('An error occurred: %s', exc_info=error)
+            logger.error('An error occurred: %s', exc_info=error)
             raise error
 
         names = map(itemgetter('title'), res['items'])
         try:
             idx = names.index(opts['title'])
         except ValueError:
-            logging.error('file name: %s not in list', opts['title'])
+            logger.error('file name: %s not in list', opts['title'])
             sys.exit()
         if idx == -1:
             raise ValueError('name %s not found in Google Drive' % opts['title'])
@@ -209,31 +151,31 @@ def get_credentials(flow, opts):
 
     #pprint.pprint(json.loads(credentials.to_json()), indent=2)
     if credentials.access_token_expired:
-        logging.info('refreshing token')
+        logger.info('refreshing token')
         refresh_http = httplib2.Http()
         credentials.refresh(refresh_http) 
     return credentials
 
 
 def download(service, file):
-    logging.debug('file.viewkeys(): %s', pprint.pformat(file.viewkeys()))
+    logger.debug('file.viewkeys(): %s', pprint.pformat(file.viewkeys()))
     #download_url = file.get('downloadUrl') # not present for some reason
     #download_url_pdf = file.get('exportLinks')['application/pdf']
     download_url = file.get('exportLinks')['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
     #download_url = re.sub('pdf$', 'csv', download_url_pdf)
     #download_url = re.sub('&exportFormat=csv$', '#gid=0&exportFormat=csv', download_url)
-    #logging.debug('file.get(\'exportlinks\': %s', file.get('exportLinks'))
+    #logger.debug('file.get(\'exportlinks\': %s', file.get('exportLinks'))
     if download_url:
         resp, content = service._http.request(download_url)
         if resp.status == 200:
-            logging.debug('Status: %s', resp)
+            logger.debug('Status: %s', resp)
             return content
         else:
-            logging.error('An error occurred: %s' % resp)
+            logger.error('An error occurred: %s' % resp)
             return None
     else:
         # The file doesn't have any content stored on Drive.
-        logging.error('file does not have any content stored on Drive')
+        logger.error('file does not have any content stored on Drive')
         return None    
 
 
@@ -241,12 +183,12 @@ def merge_config(opts, yaml_name):
     try:
         with open(yaml_name, 'r') as f:
             config = yaml.load(f)
-            logging.debug('merging command-line opts with config from file: %s', yaml_name)
+            logger.debug('merging command-line opts with config from file: %s', yaml_name)
             for k, v in config.items():
                 if not hasattr(opts,k) or getattr(opts, k) is None:
                     setattr(opts,k,v)
     except IOError:
-        logging.error('Could not find config file at %s', yaml_name)
+        logger.error('Could not find config file at %s', yaml_name)
         sys.exit()
 
 
@@ -313,12 +255,12 @@ def main():
     """
     logging set up
     """
-    root_logger = logging.getLogger()
+    logger = logging.getLogger(__name__)
     ch = logging.StreamHandler()
     formatter = logging.Formatter('[%(levelname)s]\t[%(filename)s:%(funcName)s:%(lineno)d]\t%(message)s')
     ch.setFormatter(formatter)
-    root_logger.addHandler(ch)
-    root_logger.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
 
     content = get_file(as_dict=False, **parse_args())
     write_to_stdout(content)
