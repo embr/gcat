d8e4cb8844caeb240157a62a888bf91220d66732
Evan Rosen <erosen@wikimedia.org>
2013-01-11 10:53:21 -0800

    adds support for uploading to PyPi using pypandoc for converting markdown to rst

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 
 from setuptools import setup
+import sys
+
+
+if __name__ == '__main__' and len(sys.argv) > 1 and sys.argv[1] == 'register':
+    import pypandoc
+    long_desc = pypandoc.convert('README.md', 'rst')
+else:
+    long_desc = ''
 
 setup(
     name='gcat',
     version='0.1.0',
-    description='Command-line interface for Google Drive',
+    description='OAuth wrapper and command line utility for interacting with Google Drive spreasheets',
+    long_description=long_desc,
     url='http://www.github.com/embr/gcat',
     author='Evan Rosen',
-    author_email='erosen@wikimedia.org',
+    author_email='evnrsn@gmail.com',
     entry_points = {
         'console_scripts': [
             'gcat = gcat:main',
@@ -20,6 +29,16 @@ setup(
        "httplib2 >= 0.7.6",
        "pandas >= 0.9.0"
        ],
-    include_package_data = True
+    include_package_data = True,
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ]
     )
 
